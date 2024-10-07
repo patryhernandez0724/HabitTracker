26b60e122ba7ea77c0c6bf9de5cb9da6c481b0b9
Patricia Hernandez <patryhernandez0724@gmail.com>
2024-10-07 11:32:42 -0400

    Initial Commit

---
+++ HabitTracker/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HabitTracker
+//
+//  Created by Patricia on 10/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+   
+    
+    @IBOutlet weak var gradientView: GradientView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+    }
+}
+
+
+
+
